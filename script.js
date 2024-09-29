5e2be31b688876685637a53e3f722d5314434adb
kexar007 <kexar007@gmail.com>
2024-09-30 03:07:32 +0530

    add scene object camera

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('javascript is working!!!!!');
+import * as THREE from 'three'
+
+const canvas = document.querySelector('canvas.canvas');
+
+//scene
+const scene = new THREE.Scene();
+
+//objects
+const cubegeo= new THREE.BoxGeometry(1,1,1);
+const material=new THREE.MeshBasicMaterial({color:'red'});
+const cube=new THREE.Mesh(cubegeo,material);
+scene.add(cube);
+
+//camera
+const sizes = {
+    width:800,
+    height:600
+}
+const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height);
+camera.position.z=3;
+scene.add(camera);
+
+//renderer
+const renderer=new THREE.WebGLRenderer({
+    canvas:canvas
+});
+renderer.setSize(sizes.width,sizes.height);
+renderer.render(scene,camera);
